56a32318ab437aef9ca454c5aa91f510cb51a17c
Fabian <fabian.paul.gurtner@gmail.com>
2020-08-26 21:48:59 +0200

    Use shorter parameter names to prevent name collisions with types

--- irc/commands.go
+++ irc/commands.go
@@ -7,35 +7,35 @@ import (
 
 type messageHandler func(*client, message) error
 
-var notFoundHandler messageHandler = func(client *client, message message) error {
-	fmt.Printf("Unknown command: %s\n", message.command)
+var notFoundHandler messageHandler = func(c *client, m message) error {
+	fmt.Printf("Unknown command: %s\n", m.command)
 	return nil
 }
 
 var commands = map[string]messageHandler{
-	"PASS": func(client *client, message message) error {
-		if client.registered {
-			return client.sendNumeric(ERR_ALREADYREGISTRED, "Already registered")
+	"PASS": func(c *client, m message) error {
+		if c.registered {
+			return c.sendNumeric(ERR_ALREADYREGISTRED, "Already registered")
 		}
-		if message.parameters[0] == nil {
-			return client.sendNumeric(ERR_NEEDMOREPARAMS, "No password given")
+		if m.parameters[0] == nil {
+			return c.sendNumeric(ERR_NEEDMOREPARAMS, "No password given")
 		}
 		//currently there is no auth so all passwords are allowed
 		return nil
 	},
-	"NICK": func(client *client, message message) error {
-		if message.parameters[0] == nil {
-			return client.sendNumeric(ERR_NONICKNAMEGIVEN, "No nickname given")
+	"NICK": func(c *client, m message) error {
+		if m.parameters[0] == nil {
+			return c.sendNumeric(ERR_NONICKNAMEGIVEN, "No nickname given")
 		}
-		client.nickname = message.parameters[0]
-		client.registered = true
+		c.nickname = m.parameters[0]
+		c.registered = true
 
 		var errors [5]error
-		errors[0] = client.sendNumeric(RPL_WELCOME, fmt.Sprintf("Welcome to the Internet Relay Network %s!%s@%s", *client.nickname, *client.nickname, client.server.Name))
-		errors[1] = client.sendNumeric(RPL_YOURHOST, fmt.Sprintf("Your host is %s, running version git", client.server.Name))
-		errors[2] = client.sendNumeric(RPL_CREATED, "This server was created sometime")
-		errors[3] = client.sendNumeric(RPL_MYINFO, fmt.Sprintf("%s git", client.server.Name))
-		errors[4] = client.sendNumeric(RPL_MOTDSTART, fmt.Sprintf(":- %s Message of the day - ", client.server.Name))
+		errors[0] = c.sendNumeric(RPL_WELCOME, fmt.Sprintf("Welcome to the Internet Relay Network %s!%s@%s", *c.nickname, *c.nickname, c.server.Name))
+		errors[1] = c.sendNumeric(RPL_YOURHOST, fmt.Sprintf("Your host is %s, running version git", c.server.Name))
+		errors[2] = c.sendNumeric(RPL_CREATED, "This server was created sometime")
+		errors[3] = c.sendNumeric(RPL_MYINFO, fmt.Sprintf("%s git", c.server.Name))
+		errors[4] = c.sendNumeric(RPL_MOTDSTART, fmt.Sprintf(":- %s Message of the day - ", c.server.Name))
 
 		for _, err := range errors {
 			if err != nil {
@@ -43,26 +43,26 @@ var commands = map[string]messageHandler{
 			}
 		}
 
-		for _, line := range strings.Split(*client.server.Motd, "\n") {
-			err := client.sendNumeric(RPL_MOTD, fmt.Sprintf(":- %s", line))
+		for _, line := range strings.Split(*c.server.Motd, "\n") {
+			err := c.sendNumeric(RPL_MOTD, fmt.Sprintf(":- %s", line))
 			if err != nil {
 				return err
 			}
 		}
 
-		return client.sendNumeric(RPL_ENDOFMOTD, ":End of MOTD command")
+		return c.sendNumeric(RPL_ENDOFMOTD, ":End of MOTD command")
 	},
-	"USER": func(client *client, message message) error {
-		if client.registered {
-			return client.sendNumeric(ERR_ALREADYREGISTRED, "Already registered")
+	"USER": func(c *client, m message) error {
+		if c.registered {
+			return c.sendNumeric(ERR_ALREADYREGISTRED, "Already registered")
 		}
-		if message.parameters[3] == nil {
-			return client.sendNumeric(ERR_NEEDMOREPARAMS, "Not enough parameters")
+		if m.parameters[3] == nil {
+			return c.sendNumeric(ERR_NEEDMOREPARAMS, "Not enough parameters")
 		}
-		return client.sendNumeric(ERR_USERSDISABLED, "Users are not implemented")
+		return c.sendNumeric(ERR_USERSDISABLED, "Users are not implemented")
 	},
-	"QUIT": func(client *client, message message) error {
-		client.connection.Close()
+	"QUIT": func(c *client, m message) error {
+		c.connection.Close()
 		return nil
 	},
 }
